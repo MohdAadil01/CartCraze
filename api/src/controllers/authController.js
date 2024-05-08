e10d5821a02a7259558cf14ed978cab9caf6ff01
MohdAadil01 <mohdaadil4240@gmail.com>
2024-05-08 18:16:16 +0530

    encrypt password

--- api/src/controllers/authController.js
+++ api/src/controllers/authController.js
@@ -1,7 +1,12 @@
-export const login = async (req, res) => {
-  res.send("hi");
-};
+import bcrypt from "bcrypt";
 
 export const register = async (req, res) => {
-  res.send("register route");
+  const { username, email, password, address, phone } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    res.send(hashedPassword);
+  } catch (error) {}
+};
+export const login = async (req, res) => {
+  res.send("hi");
 };
