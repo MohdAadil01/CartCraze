1785bbe1b16f5e3e3367770ec8f3c0228688618e
MohdAadil01 <mohdaadil4240@gmail.com>
2024-05-08 22:51:45 +0530

    add email,pass, phone validation

--- api/src/controllers/authController.js
+++ api/src/controllers/authController.js
@@ -7,6 +7,41 @@ export const register = async (req, res, next) => {
   if (!username || !email || !password || !address || !phone) {
     return next(createHttpError(400, "Please Enter all fields."));
   }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const passwordRegex =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[a-zA-Z]).{8,}$/;
+  const phoneRegex = /^\d{10}$/;
+  if (!emailRegex.test(email)) {
+    return next(createHttpError(400, "Please Enter a valid email address."));
+  }
+  if (!passwordRegex.test(password)) {
+    return next(
+      createHttpError(
+        400,
+        "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character."
+      )
+    );
+  }
+  if (username.length < 3 || username.length > 30) {
+    return next(
+      createHttpError(
+        400,
+        "Username must be in between 3 and 30 characters long."
+      )
+    );
+  }
+  if (!phoneRegex.test(phone)) {
+    return next(
+      createHttpError(400, "Please provide a valid 10-digit phone number.")
+    );
+  }
+  if (address.length < 5 || address.length > 100) {
+    return next(
+      createHttpError(400, "Address must be between 5 and 100 characters long.")
+    );
+  }
+
   try {
     const foundUser = await User.findOne({ email });
     if (foundUser) {
