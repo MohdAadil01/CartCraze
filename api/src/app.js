e1687b090a9be948bd41794e4b097701faf0036e
MohdAadil01 <mohdaadil4240@gmail.com>
2024-05-08 18:00:48 +0530

    set up auth route

--- api/src/app.js
+++ api/src/app.js
@@ -1,15 +1,19 @@
 import express from "express";
 import cors from "cors";
 import ErrorHandler from "./middlewares/ErrorHandler.js";
+import authRoute from "./routes/authRoute.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res, next) => {
   res.json({ message: "Testing..." });
 });
+
+app.use("/api/v1/auth", authRoute);
 
 app.use(ErrorHandler);
 export default app;
---
+++ api/src/controllers/authController.js
@@ -0,0 +1,7 @@
+export const login = async (req, res) => {
+  res.send("hi");
+};
+
+export const register = async (req, res) => {
+  res.send("register route");
+};
